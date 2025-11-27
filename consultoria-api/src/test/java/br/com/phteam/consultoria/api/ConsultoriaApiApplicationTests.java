5f953a89d17d0360c782b602216fae123e5e63f4
JoaoZ-Gomes <jgjoaogomespro@gmail.com>
2025-11-27 13:55:26 -0300

    ADD BACK END

---
+++ consultoria-api/src/test/java/br/com/phteam/consultoria/api/ConsultoriaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.phteam.consultoria.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultoriaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
